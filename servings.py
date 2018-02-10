c0d45f6ba5733880793aafd18714f27198c1f6e7
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2018-02-10 20:44:41 +0100

    Implement servings class.

---
+++ servings.py
@@ -0,0 +1,34 @@
+import re
+
+class Servings:
+    def __init__(self):
+        self.rep = {'': 1}
+
+    def __parse_rep(self, s):
+        if isinstance(s, int):
+            return "", s
+        p = re.compile('(\d+)\s*([a-z]*)')
+        m = p.search(s)
+        return m.group(2), int(m.group(1))
+
+    def update(self, l):
+        for s in l:
+            unit, amount = self.__parse_rep(s)
+            self.rep[unit] = amount
+
+    def get_factor(self, s):
+        unit, amount = self.__parse_rep(s)
+        assert unit in self.rep
+
+        return amount / self.rep[unit]
+
+    def to_list(self):
+        res = []
+        for unit, amount in self.rep.items():
+            if unit == "":
+                res.append(amount)
+            else:
+                res.append("{}{}".format(amount, unit))
+
+        return res
+
