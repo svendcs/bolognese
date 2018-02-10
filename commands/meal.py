32668ece85096646bbc9e8de7204bf8e9fbb99a1
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2018-02-10 23:44:33 +0100

    Implement food command.

--- commands/meal.py
+++ commands/meal.py
@@ -3,12 +3,13 @@ import os
 import sys
 import subprocess
 import yaml
+from meal import Meal
+from food import Food
 from dictionary_helpers import update_dictionary
 
-defaults = {'foods': [], 'servings': []}
-
 def meal_path(meal):
-    return os.path.join(constants.MEALS_DIR, meal + constants.EXTENSION)
+    for f in Meal.list():
+        print(f)
 
 def root_handle(args):
     for root, dirs, files in os.walk(constants.MEALS_DIR):
@@ -19,54 +20,73 @@ def root_handle(args):
                 print(root[len(constants.MEALS_DIR)+1:] + '/' + f)
 
 def edit_handle(args):
-    path = meal_path(args.meal)
+    meal = Meal(args.meal)
     vargs = vars(args)
 
-    if not os.path.isfile(path):
-        print("The meal '{}' does not exist.".format(args.meal), file=sys.stderr)
-        return
-
-    should_update = any(vargs[k] is not None for k in defaults.keys())
-    if should_update:
-        with open(constants.CONFIG_PATH, mode='r') as f:
-            a = yaml.safe_load(f) or {}
-        d = update_dictionary(defaults, a, vargs)
-        with open(constants.CONFIG_PATH, mode='w') as f:
-            yaml.dump(d, f, default_flow_style=False)
+    if args.servings is None:
+        subprocess.call([constants.EDITOR, meal.path()])
     else:
-        subprocess.call([constants.EDITOR, constants.CONFIG_PATH])
+        if meal.exists():
+            meal.load()
+        meal.update(vargs)
+        meal.save()
 
 def edit_register(parent):
     edit_parser = parent.add_parser('edit')
     edit_parser.set_defaults(func=edit_handle)
     edit_parser.add_argument('meal', type=str, help='Set the number of foo')
-    edit_parser.add_argument('--food', type=str, nargs='+', help='Set the number of foo')
     edit_parser.add_argument('--servings', type=str, nargs='+', help='Set the number of foo')
 
 def add_handle(args):
-    path = meal_path(args.meal)
+    meal = Meal(args.meal)
     vargs = vars(args)
 
-    if os.path.exists(path):
+    if meal.exists():
         print("The meal '{}' already exists.".format(args.meal), file=sys.stderr)
         return
 
-    should_update = any(vargs[k] is not None for k in defaults.keys())
-    if should_update:
-        with open(constants.CONFIG_PATH, mode='r') as f:
-            a = yaml.safe_load(f) or {}
-        d = update_dictionary(defaults, a, vargs)
-        with open(constants.CONFIG_PATH, mode='w') as f:
-            yaml.dump(d, f, default_flow_style=False)
+    if args.servings is None:
+        subprocess.call([constants.EDITOR, meal.path()])
     else:
-        subprocess.call([constants.EDITOR, constants.CONFIG_PATH])
+        meal.load()
+        meal.update(vargs)
+        meal.save()
 
 def add_register(parent):
-    add_parser = parent.add_parser('add')
-    add_parser.set_defaults(func=add_handle)
-    add_parser.add_argument('meal', type=str, help='Set the number of foo')
-    add_parser.add_argument('--food', type=str, nargs='+', help='Set the number of foo')
-    add_parser.add_argument('--servings', type=str, nargs='+', help='Set the number of foo')
+    parser = parent.add_parser('edit')
+    parser.set_defaults(func=add_handle)
+    parser.add_argument('meal', type=str, help='Set the number of foo')
+    parser.add_argument('--servings', type=str, nargs='+', help='Set the number of foo')
+
+def add_food_handle(args):
+    meal = Meal(args.meal)
+    food = Food(args.food)
+    vargs = vars(args)
+
+    if not meal.exists():
+        print("The meal '{}' does not exist.".format(args.meal), file=sys.stderr)
+        return
+
+    if not food.exists():
+        print("The food '{}' does not exist.".format(args.food), file=sys.stderr)
+        return
+
+    food.load()
+
+    if not food.servings.is_valid_serving(args.serving):
+        print("The serving '{}' is not a valid serving for the given food.".format(args.serving), file=sys.stderr)
+        return
+
+    meal.load()
+    meal.add_food(args.food, args.serving)
+    meal.save()
+
+def add_food_register(parent):
+    parser = parent.add_parser('add-food')
+    parser.set_defaults(func=add_food_handle)
+    parser.add_argument('meal', type=str, help='Set the number of foo')
+    parser.add_argument('food', type=str, help='Set the number of foo')
+    parser.add_argument('serving', nargs="?", type=str, default='1', help='Set the number of foo')
 
 def log_handle(args):
     print("Log handler")
@@ -77,27 +97,14 @@ def log_register(parent):
     log_parser.add_argument('meal', type=str, help='Set the number of foo')
     log_parser.add_argument('amount', type=str, help='Set the number of foo')
 
-def remove_handle(args):
-    path = meal_path(args.meal)
-
-    if not os.path.isfile(path):
-        print("The meal '{}' does not exist.".format(args.meal), file=sys.stderr)
-    else:
-        os.remove(path)
-
-def remove_register(parent):
-    remove_parser = parent.add_parser('remove')
-    remove_parser.set_defaults(func=remove_handle)
-    remove_parser.add_argument('meal', type=str, help='Set the number of foo')
-
 def register(parent):
     parser = parent.add_parser('meal', help='meal help')
     parser.set_defaults(func=root_handle)
     subparsers = parser.add_subparsers(help='subsub parser help')
 
     edit_register(subparsers)
-    remove_register(subparsers)
     log_register(subparsers)
     add_register(subparsers)
+    add_food_register(subparsers)
 
     # remove, add, edit, log
